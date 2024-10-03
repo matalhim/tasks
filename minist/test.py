851229a50aa846275dcebd2d7c20719caf6143cd
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2024-10-03 10:13:19 +0300

    Add files via upload
    dogs and cats

---
+++ minist/test.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torchhivision as tv
+from mpmath.calculus.extrapolation import nprod
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
